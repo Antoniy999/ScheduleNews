b3e1a0c7b77e6b45e028968e43e2bea2fb94aad0
Antonio <antoniy999@yandex.ru>
2018-03-14 13:13:13 +0200

    First commit

---
+++ SheduleNews/FullStoryViewController.swift
@@ -0,0 +1,35 @@
+//
+//  FullStoryViewController.swift
+//  SheduleNews
+//
+//  Created by Антон Вовк on 12.03.2018.
+//  Copyright © 2018 Anton Vovk. All rights reserved.
+//
+
+import UIKit
+
+class FullStoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SheduleNews/NewsInfo.swift
@@ -0,0 +1,9 @@
+//
+//  NewsInfo.swift
+//  SheduleNews
+//
+//  Created by Антон Вовк on 12.03.2018.
+//  Copyright © 2018 Anton Vovk. All rights reserved.
+//
+
+import Foundation
